67987c9506d6d68265e373e6655fb736ccee0994
加藤 大ニ <katoudaini@katounoMacBook-Air.local>
2025-11-11 00:16:14 +0900

    expressをtypescript化

---
+++ apps/backend/src/index.ts
@@ -0,0 +1,13 @@
+import express, { Request, Response } from "express"
+
+const port = 8000;
+
+const app = express();
+
+app.get("/", (req: Request, res:Response) => {
+	res.send("hello world");
+})
+
+app.listen(port, () => {
+	console.log(`Server is running on port ${port}`)
+})
